75396c6ab9c166607e2b90ba333500ae6dc14fce
Yannis <johnmenex@gmail.com>
2017-05-31 16:47:02 +0300

    Edited About form

--- MoCST/about.Designer.cs
+++ MoCST/about.Designer.cs
@@ -40,10 +40,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
@@ -67,9 +65,9 @@
             this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(21, 0);
             this.linkLabel1.Location = new System.Drawing.Point(4, 70);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(151, 23);
+            this.linkLabel1.Size = new System.Drawing.Size(139, 23);
             this.linkLabel1.TabIndex = 2;
-            this.linkLabel1.Text = "Α.Τ.Ε.Ι. Θεσσαλονίκης";
+            this.linkLabel1.Text = "A.T.E.I. Thessaloniki";
             this.linkLabel1.UseCompatibleTextRendering = true;
             // 
             // linkLabel2
@@ -79,24 +77,23 @@
             this.linkLabel2.LinkArea = new System.Windows.Forms.LinkArea(0, 26);
             this.linkLabel2.Location = new System.Drawing.Point(4, 90);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(178, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(167, 15);
             this.linkLabel2.TabIndex = 3;
             this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "Τμήμα Τεχνολογίας Τροφίμων -";
-            this.linkLabel2.UseCompatibleTextRendering = true;
+            this.linkLabel2.Text = "Food Technology Department";
             this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
             this.linkLabel3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.linkLabel3.LinkArea = new System.Windows.Forms.LinkArea(0, 28);
-            this.linkLabel3.Location = new System.Drawing.Point(180, 90);
+            this.linkLabel3.LinkArea = new System.Windows.Forms.LinkArea(0, 33);
+            this.linkLabel3.Location = new System.Drawing.Point(177, 90);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(187, 15);
+            this.linkLabel3.Size = new System.Drawing.Size(203, 15);
             this.linkLabel3.TabIndex = 4;
             this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "Τμήμα Μηχανικών Αυτοματισμού";
+            this.linkLabel3.Text = "Automation Engineering Department";
             this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
             // 
             // label1
@@ -105,9 +102,9 @@
             this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label1.Location = new System.Drawing.Point(1, 110);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(388, 15);
+            this.label1.Size = new System.Drawing.Size(223, 30);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Ελαιοτριβείο Α.Τ.Ε.Ι.Θ. - On line καταγραφή και επισκόπηση δεδομένων.";
+            this.label1.Text = "Olive Oil Production Facility Α.Τ.Ε.Ι.TH. \r\nOn line data monitoring and preview";
             // 
             // label2
             // 
@@ -115,9 +112,9 @@
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label2.Location = new System.Drawing.Point(1, 150);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.Size = new System.Drawing.Size(67, 15);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Μέλη ΕΠ:";
+            this.label2.Text = "Professors";
             // 
             // label4
             // 
@@ -125,9 +122,9 @@
             this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label4.Location = new System.Drawing.Point(1, 230);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.Size = new System.Drawing.Size(59, 15);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Φοιτητές:";
+            this.label4.Text = "Students:";
             // 
             // label3
             // 
@@ -135,9 +132,9 @@
             this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label3.Location = new System.Drawing.Point(1, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 15);
+            this.label3.Size = new System.Drawing.Size(94, 15);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Καλογιάννη Ελένη";
+            this.label3.Text = "Kalogianni Eleni";
             // 
             // label5
             // 
@@ -145,9 +142,9 @@
             this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label5.Location = new System.Drawing.Point(1, 180);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(127, 15);
+            this.label5.Size = new System.Drawing.Size(112, 15);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Στεργιόπουλος Φώτης";
+            this.label5.Text = "Stergiopoulos Fotis";
             // 
             // label6
             // 
@@ -155,9 +152,9 @@
             this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label6.Location = new System.Drawing.Point(1, 195);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(120, 15);
+            this.label6.Size = new System.Drawing.Size(104, 15);
             this.label6.TabIndex = 11;
-            this.label6.Text = "Μπεχτσής Δημήτριος";
+            this.label6.Text = "Bechtsis Dimitrios";
             // 
             // label7
             // 
@@ -165,19 +162,9 @@
             this.label7.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label7.Location = new System.Drawing.Point(1, 259);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(131, 15);
+            this.label7.Size = new System.Drawing.Size(115, 15);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Κατικαρίδης Δημήτριος";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label8.Location = new System.Drawing.Point(2, 274);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(126, 15);
-            this.label8.TabIndex = 13;
-            this.label8.Text = "Μανωλιός Αθανάσιος";
+            this.label7.Text = "Katikaridis Dimitrios";
             // 
             // label9
             // 
@@ -185,77 +172,66 @@
             this.label9.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label9.Location = new System.Drawing.Point(1, 245);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(100, 15);
+            this.label9.Size = new System.Drawing.Size(98, 15);
             this.label9.TabIndex = 12;
-            this.label9.Text = "Μενεξές Ιωάννης";
+            this.label9.Text = "Menexes Ioannis";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(129, 245);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(183, 13);
+            this.label10.Size = new System.Drawing.Size(189, 13);
             this.label10.TabIndex = 15;
-            this.label10.Text = "-   Τμήμα Μηχανικών Αυτοματισμού";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(130, 275);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(183, 13);
-            this.label11.TabIndex = 16;
-            this.label11.Text = "-   Τμήμα Μηχανικών Αυτοματισμού";
+            this.label10.Text = "-   Automation Engineering Department";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Location = new System.Drawing.Point(129, 261);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(183, 13);
+            this.label12.Size = new System.Drawing.Size(189, 13);
             this.label12.TabIndex = 17;
-            this.label12.Text = "-   Τμήμα Μηχανικών Αυτοματισμού";
+            this.label12.Text = "-   Automation Engineering Department";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
             this.label13.Location = new System.Drawing.Point(129, 166);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(168, 13);
+            this.label13.Size = new System.Drawing.Size(160, 13);
             this.label13.TabIndex = 18;
-            this.label13.Text = "-   Τμήμα Τεχνολογίας Τροφίμων";
+            this.label13.Text = "-   Food Technology Department";
             // 
             // label14
             // 
             this.label14.AutoSize = true;
             this.label14.Location = new System.Drawing.Point(129, 182);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(183, 13);
+            this.label14.Size = new System.Drawing.Size(189, 13);
             this.label14.TabIndex = 19;
-            this.label14.Text = "-   Τμήμα Μηχανικών Αυτοματισμού";
+            this.label14.Text = "-   Automation Engineering Department";
             // 
             // label15
             // 
             this.label15.AutoSize = true;
             this.label15.Location = new System.Drawing.Point(129, 197);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(183, 13);
+            this.label15.Size = new System.Drawing.Size(189, 13);
             this.label15.TabIndex = 20;
-            this.label15.Text = "-   Τμήμα Μηχανικών Αυτοματισμού";
+            this.label15.Text = "-   Automation Engineering Department";
             // 
             // about
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 315);
+            this.ClientSize = new System.Drawing.Size(393, 287);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -271,7 +247,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "about";
-            this.Text = "Σχετικά με την εφαρμογή";
+            this.Text = "About";
             this.Load += new System.EventHandler(this.about_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -292,10 +268,8 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label14;
